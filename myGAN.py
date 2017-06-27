9344e084b8460cd47b81cae4bf992b72e8cda436
SDDON <kiwi3618@naver.com>
2017-06-27 19:16:24 +0900

    asdas

---
+++ myGAN.py
@@ -0,0 +1,40 @@
+# 무지개 사진 (B)
+# 무지개 그림 (A)  => frequency domain (A_Freq)  -- transfer
+# Edge of A (E_A) -- edge detector
+# Edge of B (E_B)
+# E_A & E_B => GAN => E_A --> E_B -- gan
+
+# E_B + A_Freq ~= B ?
+
+
+# input  data : image data
+# return data : edge of input data
+def edge_scanner():
+    return None
+
+
+# input data  : image data
+# return data : frequency domain data
+def freq_transfer():
+    return None
+
+
+
+## GAN
+
+def generator():
+    return None
+
+def discriminator():
+    return None
+
+
+## GAN
+
+
+
+
+
+
+if __name__ == "_main_":
+    print("hi")
